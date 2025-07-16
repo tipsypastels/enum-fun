1c51081cdc43509d8ed5383a3f6889a3296282ba
tipsypastels <dakotasankey@gmail.com>
2025-07-16 11:24:47 -0700

    move doc into the macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../README.md")]
+//! Silly enum helpers.
 
 #[cfg(feature = "name-trait")]
 pub trait Name {
@@ -17,7 +17,107 @@ pub trait NamePlural: Name {
     fn name_pluralized(&self, count: usize) -> &'static str;
 }
 
-pub use enum_fun_macros::{Name, Variants};
+/// Generate a method `name(&self) -> &'static str` that
+/// returns the name of a given enum variant.
+///
+/// If the `name-trait` feature is enabled, an implementation
+/// of a `Name` trait is generated. Otherwise, an inherent
+/// implementation is used.
+///
+/// The name of an enum variant is its identifier in Title Case.
+/// `Foo` becomes `"Foo"` and `HelloWorld` becomes `"Hello World`.
+/// This can be overridden by using the `#[name = "..."]` attribute
+/// on a variant, e.g.:
+///
+/// ```rust
+/// use enum_fun::Name;
+///
+/// #[derive(Name)]
+/// enum Words {
+///     Foo,
+///     #[name = "Baz"]
+///     Bar,
+/// }
+///
+/// assert_eq!(Words::Foo.name(), "Foo");
+/// assert_eq!(Words::Bar.name(), "Baz");
+/// ```
+pub use enum_fun_macros::Name;
 
+/// Generate a method `name_plural(&self) -> &'static str` that
+/// returns the pluralized name of a given enum variant.
+///
+/// If the `name-trait` feature is enabled, an implementation
+/// of a `NamePlural` trait is generated. Otherwise,
+/// an inherent implementation is used.
+///
+/// The plural name of an enum variant is the same name that
+/// [`derive(Name)`](derive@Name) gave it, with an additional
+/// `"s"` appended. `Foo` becomes `"Foos"` and `HelloWorld`
+/// becomes `"Hello Worlds"`. This can be overridden by using
+/// the `#[name(plural = "...")]` attribute on a variant, e.g.:
+///
+/// ```rust
+/// use enum_fun::{Name, NamePlural};
+///
+/// #[derive(Name, NamePlural)]
+/// enum Words {
+///     Foo,
+///     #[name = "Baz"]
+///     Bar,
+///     #[name(plural = "Quuxes")]
+///     Quux,
+/// }
+///
+/// assert_eq!(Words::Foo.name_plural(), "Foos");
+/// assert_eq!(Words::Bar.name_plural(), "Bazs");
+/// assert_eq!(Words::Quux.name_plural(), "Quuxes");
+/// ```
+///
+/// A utility function `name_pluralized(&self, usize) -> &'static str`
+/// is also generated. It will return `name()` if the given
+/// value is `1`, and `name_plural()` otherwise. It does not
+/// prepend the provided number to the string.
+///
+/// ```rust
+/// # use enum_fun::{Name, NamePlural};
+/// # #[derive(Name, NamePlural)]
+/// # enum Words {
+/// #     Foo,
+/// # }
+/// assert_eq!(Words::Foo.name_pluralized(1), "Foo");
+/// assert_eq!(Words::Foo.name_pluralized(2), "Foos");
+/// ```
+///
+/// If the `name-includes-plural` feature is enabled,
+/// `NamePlural` does not exist and all of its features
+/// are part of [`Name`](derive@Name) instead. You may use
+/// this if all your named enums must also support
+/// pluralization, to save a derive.
 #[cfg(not(feature = "name-includes-plural"))]
 pub use enum_fun_macros::NamePlural;
+
+/// Generates inherent constants and an iterator method
+/// to enable iterating and indexing the enum variants.
+///
+/// ```rust
+/// use enum_fun::Variants;
+///
+/// #[derive(Debug, PartialEq, Variants)]
+/// enum Words {
+///     Foo,
+///     Bar,
+///     Baz,
+/// }
+///
+/// use Words::*;
+///
+/// assert_eq!(Words::VARIANT_COUNT, 3);
+/// assert_eq!(Words::VARIANTS, [Foo, Bar, Baz]);
+/// assert_eq!(Words::variants().collect::<Vec<_>>(), vec![Foo, Bar, Baz]);
+/// ```
+///
+/// The return type of the `variants()` method is an `impl ExactSizeIterator<Item = Self>`.
+///
+/// Only enums without fields are supported.
+pub use enum_fun_macros::Variants;
